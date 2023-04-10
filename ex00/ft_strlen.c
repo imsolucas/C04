46efe5301b61f1d5b5502203936e6d06d32eec7e
Lucas <denglucasyijin@gmail.com>
2023-04-10 22:18:02 +0800

    C04 work at home

---
+++ ex00/ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: djin <djin@42.fr>                          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/09 11:14:07 by djin              #+#    #+#             */
+/*   Updated: 2023/04/09 20:27:07 by djin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return(i);
+}
+
+int	main()
+{
+	char	string[] = "Hello World";
+	
+	int	count = ft_strlen(string);
+	printf("Total number is:%d", count);
+}
---
+++ ex03/ft_atoi.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: djin <djin@42.fr>                          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/10 11:11:13 by djin              #+#    #+#             */
+/*   Updated: 2023/04/10 12:51:07 by djin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	result;
+	int counter;
+	int sign;
+
+	i = 0;
+	counter = 0;
+	result = 0;
+	sign = 1;
+	while ((str[i] == ' ') || (str[i] >= 9 && str[i] <= 13))
+			i++;
+	while ((str[i] == '-') || (str[i] == '+'))
+	{
+		if (str[i] == '-')
+			counter++;
+		i++;
+	}
+	if (counter % 2 != 0)
+		sign = -1;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + str[i] - 48;
+		i++;
+	}
+	return (result * sign);
+}
+
+int	main()
+{
+	char	string[] = "---+--+1234ab567";
+	
+	ft_atoi(string);
+	printf("%d\n", ft_atoi(string));
+}
